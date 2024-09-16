7c19396679581c186ca46466694e0cdb8cb7b01e
shreyas-lyzr <141219160+shreyas-lyzr@users.noreply.github.com>
2024-09-16 16:23:20 +0530

    fix: github package url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lyzr-agent-api",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(include=["lyzr_agent_api", "lyzr_agent_api.*"]),
     install_requires=[
         "httpx==0.27.2",
@@ -11,6 +11,6 @@ setup(
     description="Official client for Lyzr Agent API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/LyzrCore/lyzr-agent",
+    url="https://github.com/LyzrCore/agent-api",
     python_requires=">=3.8.1, <3.12.5",
 )
