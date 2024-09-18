11914c12fb4c40e7cc020e22c77105463855e0b6
PrajjwalLyzr <prajjwal@lyzr.ai>
2024-09-18 22:15:49 +0530

    ThoughCast AI

---
+++ examples/ThoughtCast AI/src/utils/__init__.py
@@ -0,0 +1,6 @@
+from .utils import page_config, style_app, template_end, about_app, social_media
+
+__all__ = ['page_config',
+           'style_app',
+           'template_end',
+           'about_app']
